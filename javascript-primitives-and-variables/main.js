20ae2c75b584fe5f25e9f5663a416585cf28ad24
Cole Allen <coleallen3333@gmail.com>
2021-08-23 10:30:56 -0700

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Cole Allen';
+var isCool = true;
+var totalPets = 2;
+var unknown = null;
+var never;
+
+console.log('Full Name:', fullName);
+console.log('Is Cool?:', isCool);
+console.log('Total number of pets:', totalPets);
+console.log('Type of Full Name:', typeof fullName);
+console.log('Type of Is Cool?:', typeof isCool);
+console.log('Type of Total number of pets:', typeof totalPets);
+console.log('Unknown:', unknown);
+console.log('Type of Unknown:', typeof unknown);
+console.log('Never:', never);
+console.log('Type of Never:', typeof never);
