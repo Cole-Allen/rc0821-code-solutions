adb22141255a595c20958514d3ee881271651301
Cole Allen <coleallen3333@gmail.com>
2021-08-30 20:15:39 -0700

    Completed string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,16 +1,15 @@
 /* exported swapChars */
-// function swapChars(firstIndex, secondIndex, string) {
-//   var newString = '';
-//   var firstCup = '';
-//   var secondCup = '';
-//   for (var i = 0; i < string.length; i++) {
-//     if (i !== firstIndex && i !== secondIndex) {
-//       newString += string[i];
-//     } else if (i === firstIndex) {
-//       firstCup = string[i];
-//     } else if (i === secondIndex) {
-//       secondCup = string[i];
-//     }
-//   }
-//   return newString;
-// }
+function swapChars(firstIndex, secondIndex, string) {
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      newString += string[secondIndex];
+    } else if (i === secondIndex) {
+      newString += string[firstIndex];
+    } else {
+      newString += string[i];
+    }
+
+  }
+  return newString;
+}
