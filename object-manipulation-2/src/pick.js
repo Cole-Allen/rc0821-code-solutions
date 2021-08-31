6bdeea09274d9f381c6e105377b7fc1e80dd6b6f
Cole Allen <coleallen3333@gmail.com>
2021-08-31 10:20:02 -0700

    Not Finished

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,12 @@
+/* exported pick */
+function pick(source, keys) {
+  var obj = {};
+  for (var key in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (key === keys[i]) {
+        obj[key] = source[key];
+      }
+    }
+  }
+  return obj;
+}
