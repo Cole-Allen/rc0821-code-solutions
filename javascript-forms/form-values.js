120715a3653be3301bc132cfb42d07804874ff5b
Cole Allen <coleallen3333@gmail.com>
2021-09-01 10:46:48 -0700

    Completed javascipt-forms

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,10 @@
+var $sendButton = document.querySelector('#contact-form');
+
+$sendButton.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var obj = {};
+  obj.name = document.forms[0].name.value;
+  obj.email = document.forms[0].email.value;
+  obj.message = document.forms[0].message.value;
+  return obj;
+});
