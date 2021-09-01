08fecd1608556394c3f3f02e06f761e15b7dd698
Cole Allen <coleallen3333@gmail.com>
2021-09-01 11:16:18 -0700

    Added reset method

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,10 +1,11 @@
-var $sendButton = document.querySelector('#contact-form');
+var $forms = document.querySelector('#contact-form');
 
-$sendButton.addEventListener('submit', function (event) {
+$forms.addEventListener('submit', function (event) {
   event.preventDefault();
   var obj = {};
   obj.name = document.forms[0].name.value;
   obj.email = document.forms[0].email.value;
   obj.message = document.forms[0].message.value;
   console.log(obj);
+  $forms.reset();
 });
