349c78a42f818e28bb1a837cdcf74e164735c35c
Cole Allen <coleallen3333@gmail.com>
2021-09-02 17:41:53 -0700

    Work

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,8 +1,14 @@
 /* exported flatten */
-// function flatten(array) {
-//   for (var i = 0; i < array.length; i++) {
-//     if (array[i].length > 0) {
-
-//     }
-//   }
-// }
+function flatten(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > 0) {
+      for (var j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
