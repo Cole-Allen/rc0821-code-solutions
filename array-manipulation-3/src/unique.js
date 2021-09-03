67f9b9b1a5eb25569aefaf5ceb9c7bbca744659b
Cole Allen <coleallen3333@gmail.com>
2021-09-02 17:43:29 -0700

    Work

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,12 +1,12 @@
 /* exported unique */
 function unique(array) {
-//   var newArray = [];
-//   for (var i = 0; i < array.length; i++) {
-//     var unique = array[i];
-//     for (var j = 0; j < array.length; j++) {
-//       if (unique === array[j]) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    var unique = array[i];
+    for (var j = 0; j < array.length; j++) {
+      if (unique === array[j]) {
 
-//       }
-//     }
-//   }
-// }
+      }
+    }
+  }
+}
