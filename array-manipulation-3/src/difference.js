8908ded2573f7a7d86291394d9a44b703ebc7141
Cole Allen <coleallen3333@gmail.com>
2021-09-04 14:55:16 -0700

    Finsished difference function

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,11 +1,28 @@
 /* exported difference */
 function difference(first, second) {
   var newArray = [];
-  var shortest = first.length;
-  if (shortest > second.length) {
-    shortest = second.length;
+  for (var i = 0; i < first.length; i++) {
+    var diff = true;
+    for (var j = 0; j < second.length; j++) {
+      if (first[i] === second[j]) {
+        diff = false;
+      }
+    }
+    if (diff) {
+      newArray.push(first[i]);
+    }
   }
-  for (var i = 0; i < shortest; i++) {
 
+  for (var l = 0; l < second.length; l++) {
+    var diff2 = true;
+    for (var k = 0; k < first.length; k++) {
+      if (second[l] === first[k]) {
+        diff2 = false;
+      }
+    }
+    if (diff2) {
+      newArray.push(second[l]);
+    }
   }
+  return newArray;
 }
