a10338e1e408d75840e4004b391385a5d54aee5f
Cole Allen <coleallen3333@gmail.com>
2021-09-03 17:55:27 -0700

    Started difference function

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,11 @@
 /* exported difference */
+function difference(first, second) {
+  var newArray = [];
+  var shortest = first.length;
+  if (shortest > second.length) {
+    shortest = second.length;
+  }
+  for (var i = 0; i < shortest; i++) {
+
+  }
+}
