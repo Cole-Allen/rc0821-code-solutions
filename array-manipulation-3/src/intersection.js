55d9a367c3739e993a27953944bae45f43f87ef7
Cole Allen <coleallen3333@gmail.com>
2021-09-04 16:14:06 -0700

    Completed array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,12 @@
 /* exported intersection */
+function intersection(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    for (var j = 0; j < second.length; j++) {
+      if (first[i] === second[j]) {
+        newArray.push(first[i]);
+      }
+    }
+  }
+  return newArray;
+}
