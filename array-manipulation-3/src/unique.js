d3494180275e6f7ead031fd472da61b555ce92a7
Cole Allen <coleallen3333@gmail.com>
2021-09-03 17:41:40 -0700

    Completed the unique challenge

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -2,10 +2,18 @@
 function unique(array) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
-    for (var j = 0; j < array.length; j++) {
-      if (array[i] === array[j]) {
 
+    var unique = true;
+
+    for (var j = 0; j < newArray.length; j++) {
+
+      if (array[i] === newArray[j]) {
+        unique = false;
       }
+    }
+
+    if (unique) {
+      newArray.push(array[i]);
     }
   }
   return newArray;
