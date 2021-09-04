0c29d5b10fb9974507f174a5adfcfa44a1824eb6
Cole Allen <coleallen3333@gmail.com>
2021-09-04 15:19:26 -0700

    Finished union function

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,19 @@
 /* exported union */
+function union(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    newArray.push(first[i]);
+  }
+  for (var j = 0; j < second.length; j++) {
+    var diff = true;
+    for (var l = 0; l < first.length; l++) {
+      if (second[j] === newArray[l]) {
+        diff = false;
+      }
+    }
+    if (diff) {
+      newArray.push(second[j]);
+    }
+  }
+  return newArray;
+}
