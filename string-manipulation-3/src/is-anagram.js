9f7ae94a44c1fc47d23b8e2024c295d862fb71e5
Cole Allen <coleallen3333@gmail.com>
2021-09-06 15:47:13 -0700

    Almost got anagram function working

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,20 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  var indexes = [];
+  for (var i = 0; i < firstString.length; i++) {
+    var match = false;
+    for (var j = 0; j < secondString.length; j++) {
+      if (firstString[i] === secondString[j] && !indexes.includes(j)) {
+        indexes.push(j);
+        match = true;
+        break;
+      }
+    }
+    if (!match) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/* dang im close */
