d155f1d307592296b21ef6f7fef189409efb551d
Cole Allen <coleallen3333@gmail.com>
2021-09-06 19:20:17 -0700

    working on titleCase function

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -3,9 +3,18 @@ function titleCase(string) {
   var shortWordsString = 'and nor but or a an the as at by for in of on per to';
   var newString = '';
   var splitString = string.split(' ');
+  console.log(splitString);
   for (var i = 0; i < splitString.length; i++) {
+    if (splitString[i].toUpperCase() === 'API') {
+      newString += 'API';
+      continue;
+    }
+    if (splitString[i].toUpperCase() === 'JAVASCRIPT') {
+      newString += 'JavaScript';
+      continue;
+    }
     for (var j = 0; j < splitString[i].length; j++) {
-      if (splitString[i].length > 3 || !shortWordsString.includes(splitString[i])) {
+      if (!shortWordsString.includes(splitString[i])) {
         if (j === 0) {
           newString += splitString[i][j].toUpperCase();
         } else {
