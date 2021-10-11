3e0dc3cefb07f7c1d61fa6f3853294a1d4dff5e9
cole-allen <coleallen3333@gmail.com>
2021-10-11 14:51:47 -0700

    Completed express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -61,8 +61,8 @@ app.delete('/api/notes/:id', function (req, res, next) {
     res.status(400);
     res.send({ error: 'id is not a positive integer' });
   } else {
-    if (data.notes[req.params.id]) {
-      delete data.notes[req.params.id];
+    if (data.notes[paramID]) {
+      delete data.notes[paramID];
       fs.writeFile('./data.json', JSON.stringify(data, null, 2), 'utf-8', err => {
         if (err) {
           res.status(500);
@@ -71,6 +71,33 @@ app.delete('/api/notes/:id', function (req, res, next) {
         } else {
           res.status(204);
           res.send();
+        }
+      });
+
+    } else {
+      res.status(404);
+      res.send({ error: 'Entry not found' });
+    }
+  }
+});
+
+app.put('/api/notes/:id', function (req, res, next) {
+  const paramID = parseInt(req.params.id);
+  if (typeof paramID !== 'number' || Math.sign(paramID) === -1) {
+    res.status(400);
+    res.send({ error: 'id is not a positive integer' });
+  } else {
+    if (data.notes[paramID]) {
+      data.notes[paramID] = req.body;
+      data.notes[paramID].id = paramID;
+      fs.writeFile('./data.json', JSON.stringify(data, null, 2), 'utf-8', err => {
+        if (err) {
+          res.status(500);
+          res.send({ error: 'An unexpected error occured' });
+
+        } else {
+          res.status(200);
+          res.send(data.notes[paramID]);
         }
       });
 
