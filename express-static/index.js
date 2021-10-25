86312cc1b173d3acf17faaf7c12d17a3ac61d3e3
cole-allen <coleallen3333@gmail.com>
2021-10-25 11:10:03 -0700

    Fixed filepath

--- express-static/index.js
+++ express-static/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const file = express.static('./public');
+const file = express.static(path.join(__dirname, 'public'));
 
 app.use(file);
 
