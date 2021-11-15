3f7ff73fa206923d371c32e2740dc6d554c29c0f
cole-allen <coleallen3333@gmail.com>
2021-11-15 15:31:05 -0800

    Im terrible at math, but I tried

---
+++ big-o-notation/big-o.js
@@ -0,0 +1,44 @@
+/* exported uniqueQuadratic, uniqueLinear */
+
+function uniqueLinear(words) {
+  const seen = {};                  // 1 * 1 = O(1)
+  const unique = [];                // 1 * 1 = O(1)
+  for (
+    let i = 0;                      // 1 * 1 = O(1)
+    i < words.length;               // 2 * n = O(2n) ~= O(n)
+    i++                             // 2 * n = O(2n) ~= O(n)
+  ) {
+    const word = words[i];          // 1 * n = O(n)
+    if (!seen[word]) {              // n * n = O(n^2)
+      seen[word] = true;
+      unique[unique.length] = word; // n * n = O(n^2)
+    }
+  }
+  return unique;
+} // Big O Notation for uniqueLinear: O(n^2)
+
+function uniqueQuadratic(words) {
+  const unique = [];                // 1 * 1 = O(1)
+  for (
+    let i = 0;                      // 1 * 1 = O(1)
+    i < words.length;               // 2 * n = O(n)
+    i++                             // 2 * n = O(n)
+  ) {
+    const word = words[i];          // 1 * n = O(n)
+    let isUnique = true;            // 1 * 1 = O(1)
+    for (
+      let c = 0;                    // 1 * 1 = O(1)
+      c < i;                        // 2 * n = O(n)
+      c++                           // 2 * n = O(n)
+    ) {
+      const comparing = words[c];   // 1 * n = O(n)
+      if (comparing === word) {     // 1 * 1 = O(1)
+        isUnique = false;           // 1 * 1 = O(1)
+      }
+    }
+    if (isUnique) {                 // 1 * 1 = O(1)
+      unique[unique.length] = word; // n * 1 = O(n)
+    }
+  }
+  return unique;                    // 1 * 1 = O(1)
+} // Big O Notation for uniqueQuadratic: O(n)
